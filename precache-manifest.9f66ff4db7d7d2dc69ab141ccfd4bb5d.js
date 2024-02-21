74a411a623cf909ea9aaa85721bd4883c12ceffa
Sneha Parkar <snehaparkar007@gmail.com>
2024-02-21 16:12:45 -0500

    Deploy React app to GitHub Pages

---
+++ precache-manifest.9f66ff4db7d7d2dc69ab141ccfd4bb5d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0732763dcf41274cc4258f822c9ac406",
+    "url": "/quizzical-app/index.html"
+  },
+  {
+    "revision": "d4405a8d26bad2a7ffb9",
+    "url": "/quizzical-app/static/css/main.e514201a.chunk.css"
+  },
+  {
+    "revision": "cfa702bef7ab15b2812e",
+    "url": "/quizzical-app/static/js/2.3c453ae8.chunk.js"
+  },
+  {
+    "revision": "d4405a8d26bad2a7ffb9",
+    "url": "/quizzical-app/static/js/main.f7a20244.chunk.js"
+  },
+  {
+    "revision": "551224d1990664806ab6",
+    "url": "/quizzical-app/static/js/runtime~main.9c04eace.js"
+  }
+]);
